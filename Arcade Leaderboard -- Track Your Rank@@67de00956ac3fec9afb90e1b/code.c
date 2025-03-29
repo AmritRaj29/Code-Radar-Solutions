e103a4e5654e1f218d854dc789c4e985ba063a01
AmritRaj29 <amrit0097.becse24@chitkara.edu.in>
2025-03-29 10:14:27 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,32 +1,32 @@
 // Your code here...
 void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]){
-    int uniqueRanked[n];
-    int uniqueCount = 0;
-    for (int i = 0; i < n; i++){
-        if (i == 0 || ranked[i] != ranked[i-1]){
-            uniqueRanked[uniqueCount++] = ranked[i];
-        }
-    }
+    
     for (int i = 0; i < m; i++){
         int rank = 1;
         int j = 0;
-        while (j < uniqueCount && uniqueRanked[j] > player[i]){
+        while (j < n && ranked[j] > player[i]){
             rank++;
+            j++;
+        }
+        while (j < n && ranked[j] == player[i]){
             j++;
         }
         result[i] = rank;
     }
 }
 int main(){
-    int ranked[] = {100, 90, 90, 80, 75,60};
-    int player[] = {50, 65, 77, 90};
-    int n = sizeof(ranked)/sizeof(ranked[0]);
-    int m = sizeof(player)/sizeof(player[0]);
-    int result[m];
+    int n,m;
+    scanf("%d", &n);
+    int ranked[200000];
+    for (int i = 0; i < n; i++) scanf("%d", &ranked[i]);
+    scanf("%d", &m);
+    int player[200000];
+    for (int i = 0; i < m; i++) scanf("%d", &player[i]);
+    int result[200000];
 
     trackPlayerRanks(ranked, n, player, m, result);
-    for (int i = 0; i < m; i++){
-        printf("%d", result[i]);
-    }
+    for (int i = 0; i < m; i++)
+        printf("%d\n", result[i]);
+    
     return 0;
 }
